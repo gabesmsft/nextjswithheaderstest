61dcbbc0fbe5793f85bcb8465f7743d6d45ad2d8
Gabe Shapiro <gabes@microsoft.com>
2023-02-10 18:50:37 -0800

    1

--- next.config.js
+++ next.config.js
@@ -3,4 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },
+}
